d33178c0f1078fb101675db461e3c8a18b4b6f46
Justin Jaffray <justin.jaffray@gmail.com>
2013-04-07 20:47:59 -0400

    grouped algs

--- assets/js/alg-sorter.js
+++ assets/js/alg-sorter.js
@@ -3,7 +3,13 @@ function go() {
   var fixedAlgs = algs.map(Sorter.stripAuf);
   fixedAlgs = Sorter.fixAlgList(fixedAlgs);
   $("#result").empty();
+  highestQtm = 0
   $.each(fixedAlgs,function(index,alg){
+    if (Algorithm.qtmLength(alg) > highestQtm) {
+      highestQtm = Algorithm.qtmLength(alg);
+      $("#result").append("<br><br><h4>length " + highestQtm + "</h4>")
+      $("#result").append("<hr>")
+    }
     $("#result").append(alg);
     $("#result").append("<br>");
   });
